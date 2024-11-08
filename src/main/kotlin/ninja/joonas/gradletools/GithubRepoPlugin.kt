885fcf8f9e51c876fd1821ef042e36e371519005
Joonas Nousiainen <joonas.nousiainen@siili.com>
2024-11-08 15:55:41 +0200

    .

--- src/main/kotlin/ninja/joonas/gradletools/GithubRepoPlugin.kt
+++ src/main/kotlin/ninja/joonas/gradletools/GithubRepoPlugin.kt
@@ -2,16 +2,16 @@ package ninja.joonas.gradletools
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.credentials.PasswordCredentials
+import org.gradle.api.artifacts.repositories.PasswordCredentials
 
 class GithubRepoPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val conf = project.extensions.create("githubRepoConfig", RepositoryConf::class.java)
 
         project.afterEvaluate {
-            conf.modules.forEach { modulePath ->
+            conf.modules.forEach { repoName ->
                 project.repositories.maven {
-                    url = project.uri("https://maven.pkg.github.com/${conf.repoRoot}/$modulePath")
+                    url = project.uri("https://maven.pkg.github.com/${conf.repoRoot}/$repoName")
                     credentials(PasswordCredentials::class.java) {
                         username = project.findProperty("gpr.user") as String? ?: System.getenv("PR_ACCESS_TOKEN_USER")
                         password = project.findProperty("gpr.key") as String? ?: System.getenv("PR_ACCESS_TOKEN")
